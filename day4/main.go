abda4f338a089f6d895fe116352e92dd9fbbf948
Eric Lunt <eric@lunt.software>
2021-12-04 00:14:44 -0600

    Day 4 with 2d array

--- day4/main.go
+++ day4/main.go
@@ -9,6 +9,9 @@ import (
 	"github.com/ghonzo/advent2021/common"
 )
 
+// The board dimension
+const dim int = 5
+
 // Day 4: Giant Squid
 // Part 1 answer: 51034
 // Part 2 answer: 5434
@@ -24,8 +27,8 @@ func part1(entries []string) int {
 	numbers := strings.Split(entries[0], ",")
 	boards := make([]*Board, 0)
 	// Now make boards
-	for i := 2; i < len(entries); i += 6 {
-		boards = append(boards, createBoard(entries[i:i+5]))
+	for i := 2; i < len(entries); i += dim + 1 {
+		boards = append(boards, createBoard(entries[i:i+dim]))
 	}
 	// Now draw the numbers
 	for _, num := range numbers {
@@ -44,8 +47,8 @@ func part2(entries []string) int {
 	numbers := strings.Split(entries[0], ",")
 	boards := make([]*Board, 0)
 	// Now make boards
-	for i := 2; i < len(entries); i += 6 {
-		boards = append(boards, createBoard(entries[i:i+5]))
+	for i := 2; i < len(entries); i += dim + 1 {
+		boards = append(boards, createBoard(entries[i:i+dim]))
 	}
 	// Now draw the numbers
 	winners := make(map[int]bool)
@@ -63,7 +66,7 @@ func part2(entries []string) int {
 	panic("Didn't find a last winner")
 }
 
-type Board [][]*Space
+type Board [dim][dim]*Space
 
 type Space struct {
 	val     int
@@ -72,21 +75,19 @@ type Space struct {
 
 func createBoard(lines []string) *Board {
 	var b Board
-	for _, line := range lines {
-		spaces := make([]*Space, 0, 5)
-		for _, numStr := range strings.Fields(line) {
+	for row, line := range lines {
+		for col, numStr := range strings.Fields(line) {
 			val, _ := strconv.Atoi(numStr)
-			spaces = append(spaces, &Space{val: val})
+			b[row][col] = &Space{val: val}
 		}
-		b = append(b, spaces)
 	}
 	return &b
 }
 
 func (b *Board) Cover(v int) bool {
 	// Return true if a winner
-	var hcovered [5]int
-	var vcovered [5]int
+	var hcovered [dim]int
+	var vcovered [dim]int
 	for row, spaces := range *b {
 		for col, space := range spaces {
 			if space.val == v {
@@ -96,7 +97,7 @@ func (b *Board) Cover(v int) bool {
 				hcovered[row]++
 				vcovered[col]++
 			}
-			if hcovered[row] == 5 || vcovered[col] == 5 {
+			if hcovered[row] == dim || vcovered[col] == dim {
 				return true
 			}
 		}
